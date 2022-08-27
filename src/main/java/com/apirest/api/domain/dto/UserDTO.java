2df1b5b4e3823ca6d27c9620f51bc9d397f269f7
Karine Ogg <karineogg16@gmail.com>
2022-08-27 19:38:46 -0300

    classe user dto

---
+++ src/main/java/com/apirest/api/domain/dto/UserDTO.java
@@ -0,0 +1,19 @@
+package com.apirest.api.domain.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+
+}
