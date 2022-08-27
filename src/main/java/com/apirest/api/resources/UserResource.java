66a6bd8de450010fe8f1d7c8a34bfd3d02a02ead
Karine Ogg <karineogg16@gmail.com>
2022-08-27 16:21:21 -0300

    classe user resource findById

--- src/main/java/com/apirest/api/resources/UserResource.java
+++ src/main/java/com/apirest/api/resources/UserResource.java
@@ -2,6 +2,8 @@ package com.apirest.api.resources;
 
 
 import com.apirest.api.domain.User;
+import com.apirest.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,10 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
 
-        return ResponseEntity.ok().body(new User(1, "karine", "kari@email.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
 
 
     }
