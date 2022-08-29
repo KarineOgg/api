cfe28d07209c010780a1fa6dcf6cbcd3d9112f5f
Karine Ogg <karineogg16@gmail.com>
2022-08-29 01:00:23 -0300

    teste para manipulador de excecoes metodo object not found

---
+++ src/test/java/com/apirest/api/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -0,0 +1,51 @@
+package com.apirest.api.resources.exceptions;
+
+import com.apirest.api.services.exceptions.ObjectNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ResourceExceptionHandlerTest {
+
+    public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
+    @InjectMocks
+    private ResourceExceptionHandler exceptionHandler;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @DisplayName("deve retornar uma excecao objectNotFound quando o obj nao for encontrado")
+    @Test
+    void testarObjectNotFoundException() {
+        ResponseEntity<StandardError> response = exceptionHandler
+                .objectNotFound(
+                        new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO),
+                        new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(OBJETO_NAO_ENCONTRADO, response.getBody().getError());
+        assertEquals(404, response.getBody().getStatus());
+
+
+
+    }
+
+    @Test
+    void dataIntegratyViolationException() {
+    }
+}
