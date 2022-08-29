b9c51f3d7b42502e5c06fd3e56a31e6556101630
Karine Ogg <karineogg16@gmail.com>
2022-08-29 15:14:51 -0300

    teste para manipulador de excecoes metodo data integrity

--- src/main/java/com/apirest/api/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/apirest/api/resources/exceptions/ResourceExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.apirest.api.resources.exceptions;
 
-import com.apirest.api.services.exceptions.DataIntegratyViolationException;
+import com.apirest.api.services.exceptions.DataIntegrityViolationException;
 import com.apirest.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,8 +20,8 @@ public class ResourceExceptionHandler {
                 new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
-    @ExceptionHandler(DataIntegratyViolationException.class)
-    public ResponseEntity<StandardError>dataIntegratyViolationException(DataIntegratyViolationException ex, HttpServletRequest request) {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError>dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request) {
 
         StandardError error =
                 new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getRequestURI());
